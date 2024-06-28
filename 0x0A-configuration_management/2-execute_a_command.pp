afd4ffc9cd23a55f7bfb239a734e83ed04d7176d
Ngayep <courtesyofjess@gmail.com>
2024-06-28 19:18:26 +0100

    execute a command modified

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,6 @@
-#executes a command
-exec : { 'pkill':
-  path     => '/usr/bin'
+# kill process killmenow
+
+exec { 'pkill':
   command  => 'pkill killmenow',
-  provider => shell,
-  returns  => [0, 1]
+  provider => 'shell',
 }
