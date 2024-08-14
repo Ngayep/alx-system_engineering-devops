61a13a52080caea582ce2ef0280d32aa01fc8efe
Ngayep <courtesyofjess@gmail.com>
2024-08-14 03:30:26 +0100

    modified

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,17 +11,15 @@ def number_of_subscribers(subreddit):
     """ Queries to Reddit API """
     u_agent = 'Mozilla/5.0'
 
-    headers = {
-        'User-Agent': u_agent
-    }
+    headers = {'User-Agent': u_agent}
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    res = requests.get(url, headers=headers, allow_redirects=False)
-    if res.status_code != 200:
+    try:
+        res = requests.get(url, headers=headers, allow_redirects=False)
+        if res.status_code == 200:
+            data = response.json()
+            return data.get('data', {}).get('subscribers', 0)
+        else:
+            return 0
+    except requests.RequestException:
         return 0
-    dic = res.json()
-    if 'data' not in dic:
-        return 0
-    if 'subscribers' not in dic.get('data'):
-        return 0
-    return res.json()['data']['subscribers']
