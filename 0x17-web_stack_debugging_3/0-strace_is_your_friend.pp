116c3168f19cb2bb0c0cf5df31aa8b10867ef368
Ngayep <courtesyofjess@gmail.com>
2024-08-16 11:55:00 +0100

    puppet

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,30 +1,6 @@
-# This Puppet manifest configures Apache, ensuring the correct permissions and service status.
+# Puppet manifest to fix a bug in wp-settings.php
 
-# Ensure Apache is installed
-package { 'apache2':
-  ensure => installed,
-}
-
-# Ensure Apache service is running
-service { 'apache2':
-  ensure => running,
-  enable => true,
-}
-
-# Ensure the web root directory has the correct permissions
-file { '/var/www/html':
-  ensure => directory,
-  owner  => 'www-data',
-  group  => 'www-data',
-  mode   => '0755',
-}
-
-# Example of a configuration file fix
-file { '/etc/apache2/sites-available/000-default.conf':
-  ensure => file,
-  source => 'puppet:///modules/apache/000-default.conf',
-  owner  => 'root',
-  group  => 'root',
-  mode   => '0644',
-  notify => Service['apache2'],
+exec { 'fix the php extension issue':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
